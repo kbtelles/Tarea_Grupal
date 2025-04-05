6eac1546b669ba8c9705ca92201424fb82c41c36
lluch2019087 <davidluch059@gmail.com>
2025-04-04 23:12:33 -0600

    Subiendo el ejercicio 1

---
+++ Ejercicio1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    double num1, num2;
+
+    // Solicitar al usuario que ingrese dos números
+    cout << "Ingresa el primer numero: ";
+    cin >> num1;
+    cout << "Ingresa el segundo numero: ";
+    cin >> num2;
+
+    // Suma
+    cout << "La suma de " <<num1<< " y " <<num2<< " es: " <<num1 + num2 << endl;
+
+    // Resta
+    cout << "La resta de " <<num1<< " y " <<num2<< " es: " <<num1 - num2 << endl;
+
+    // Multiplicación
+    cout << "La multiplicacion de " <<num1<< " y " <<num2 << " es: " <<num1 * num2 << endl;
+
+    // División (Verificar que el segundo número no sea cero)
+    if (num2 != 0) {
+        cout << "La division de " <<num1<< " entre " <<num2 << " es: " <<num1 / num2 << endl;
+    } else {
+        cout << "Error: No se puede dividir entre cero." <<endl;
+    }
+
+    return 0; 
+}
+
